d6c674c82eb0aa57fa7d274ba24582ccdaf841e5
Dmitriy Krasnyansky <k.dimonych@gmail.com>
2025-11-26 00:36:36 +0200

    fix: both Sync and Async modes are supported

--- src/ds323x/status.rs
+++ src/ds323x/status.rs
@@ -1,23 +1,35 @@
 //! Device status
+maybe_async_cfg::content! {
+#![maybe_async_cfg::default(
+    idents(ReadData, WriteData, Ds323x),
+)]
 
+#[maybe_async_cfg::maybe(
+    sync(not(feature = "async")),
+    async(feature = "async")
+)]
 use crate::{
     interface::{ReadData, WriteData},
     BitFlags, Ds323x, Error, Register,
 };
 
+#[maybe_async_cfg::maybe(
+    sync(not(feature = "async")),
+    async(feature = "async")
+)]
 impl<DI, IC, E> Ds323x<DI, IC>
 where
     DI: ReadData<Error = Error<E>> + WriteData<Error = Error<E>>,
 {
     /// Read whether the oscillator is running
-    pub fn running(&mut self) -> Result<bool, Error<E>> {
-        let control = self.iface.read_register(Register::CONTROL)?;
+    pub async fn running(&mut self) -> Result<bool, Error<E>> {
+        let control = self.iface.read_register(Register::CONTROL).await?;
         Ok((control & BitFlags::EOSC) == 0)
     }
 
     /// Read the busy status
-    pub fn busy(&mut self) -> Result<bool, Error<E>> {
-        let status = self.iface.read_register(Register::STATUS)?;
+    pub async fn busy(&mut self) -> Result<bool, Error<E>> {
+        let status = self.iface.read_register(Register::STATUS).await?;
         Ok((status & BitFlags::BUSY) != 0)
     }
 
@@ -28,8 +40,8 @@ where
     ///
     /// Once this is true, it will stay as such until cleared with
     /// [`clear_has_been_stopped_flag()`](#method.clear_has_been_stopped_flag)
-    pub fn has_been_stopped(&mut self) -> Result<bool, Error<E>> {
-        let status = self.iface.read_register(Register::STATUS)?;
+    pub async fn has_been_stopped(&mut self) -> Result<bool, Error<E>> {
+        let status = self.iface.read_register(Register::STATUS).await?;
         Ok((status & BitFlags::OSC_STOP) != 0)
     }
 
@@ -37,52 +49,52 @@ where
     /// stopped at some point.
     ///
     /// See also: [`has_been_stopped()`](#method.has_been_stopped)
-    pub fn clear_has_been_stopped_flag(&mut self) -> Result<(), Error<E>> {
+    pub async fn clear_has_been_stopped_flag(&mut self) -> Result<(), Error<E>> {
         let status = self.status & !BitFlags::OSC_STOP;
-        self.write_status_without_clearing_alarm(status)
+        self.write_status_without_clearing_alarm(status).await
     }
 
     /// Read whether the Alarm1 has matched at some point.
     ///
     /// Once this is true, it will stay as such until cleared with
     /// [`clear_alarm1_matched_flag()`](#method.clear_alarm1_matched_flag)
-    pub fn has_alarm1_matched(&mut self) -> Result<bool, Error<E>> {
-        let status = self.iface.read_register(Register::STATUS)?;
+    pub async fn has_alarm1_matched(&mut self) -> Result<bool, Error<E>> {
+        let status = self.iface.read_register(Register::STATUS).await?;
         Ok((status & BitFlags::ALARM1F) != 0)
     }
 
     /// Clear flag signalling whether the Alarm1 has matched at some point.
     ///
     /// See also: [`has_alarm1_matched()`](#method.has_alarm1_matched)
-    pub fn clear_alarm1_matched_flag(&mut self) -> Result<(), Error<E>> {
+    pub async fn clear_alarm1_matched_flag(&mut self) -> Result<(), Error<E>> {
         let status = self.status | BitFlags::ALARM2F;
-        self.iface.write_register(Register::STATUS, status)
+        self.iface.write_register(Register::STATUS, status).await
     }
 
     /// Read whether the Alarm2 has matched at some point.
     ///
     /// Once this is true, it will stay as such until cleared with
     /// [`clear_alarm2_matched_flag()`](#method.clear_alarm2_matched_flag)
-    pub fn has_alarm2_matched(&mut self) -> Result<bool, Error<E>> {
-        let status = self.iface.read_register(Register::STATUS)?;
+    pub async fn has_alarm2_matched(&mut self) -> Result<bool, Error<E>> {
+        let status = self.iface.read_register(Register::STATUS).await?;
         Ok((status & BitFlags::ALARM2F) != 0)
     }
 
     /// Clear flag signalling whether the Alarm2 has matched at some point.
     ///
     /// See also: [`has_alarm2_matched()`](#method.has_alarm2_matched)
-    pub fn clear_alarm2_matched_flag(&mut self) -> Result<(), Error<E>> {
+    pub async fn clear_alarm2_matched_flag(&mut self) -> Result<(), Error<E>> {
         let status = self.status | BitFlags::ALARM1F;
-        self.iface.write_register(Register::STATUS, status)
+        self.iface.write_register(Register::STATUS, status).await
     }
 
     /// Read the temperature.
     ///
     /// Note: It is possible to manually force a temperature conversion with
     /// [`convert_temperature()`](#method.convert_temperature)
-    pub fn temperature(&mut self) -> Result<f32, Error<E>> {
+    pub async fn temperature(&mut self) -> Result<f32, Error<E>> {
         let mut data = [Register::TEMP_MSB, 0, 0];
-        self.iface.read_data(&mut data)?;
+        self.iface.read_data(&mut data).await?;
         let is_negative = (data[1] & 0b1000_0000) != 0;
         let temp = (u16::from(data[1]) << 2) | u16::from(data[2] >> 6);
         if is_negative {
@@ -92,4 +104,6 @@ where
             Ok(f32::from(temp) * 0.25)
         }
     }
+}
+
 }
